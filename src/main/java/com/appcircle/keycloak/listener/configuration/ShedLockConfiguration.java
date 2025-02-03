75e38bae09b35e979b2ad8312d1999e5b4104729
aparmaksiz_mcgh <aparmaksiz@mastercontrol.com>
2025-02-03 11:47:11 +0300

    change sched lock conf

--- src/main/java/com/appcircle/keycloak/listener/configuration/ShedLockConfiguration.java
+++ src/main/java/com/appcircle/keycloak/listener/configuration/ShedLockConfiguration.java
@@ -16,10 +16,11 @@ import javax.sql.DataSource;
 public class ShedLockConfiguration {
     @Bean
     public LockProvider lockProvider(DataSource dataSource) {
-        return new JdbcTemplateLockProvider.Configuration.builder()
-                .withJdbcTemplate(new JdbcTemplate(dataSource))
-                .usingDbTime()
-                .build();
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime()
+                        .build()
     }
 }
 
