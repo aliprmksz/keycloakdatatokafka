3c55d43b26bffa7c36dd3f790703619c9b019e37
aparmaksiz_mcgh <aparmaksiz@mastercontrol.com>
2025-02-03 11:44:33 +0300

    change sched lock conf

--- src/main/java/com/appcircle/keycloak/listener/configuration/ShedLockConfiguration.java
+++ src/main/java/com/appcircle/keycloak/listener/configuration/ShedLockConfiguration.java
@@ -2,18 +2,24 @@ package com.appcircle.keycloak.listener.configuration;
 
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "1m")
 public class ShedLockConfiguration {
     @Bean
     public LockProvider lockProvider(DataSource dataSource) {
-        return new JdbcTemplateLockProvider(dataSource);
+        JdbcTemplateLockProvider.Configuration.builder()
+                .withJdbcTemplate(new JdbcTemplate(dataSource))
+                .usingDbTime()
+                .build();
     }
 }
 
